ba3daff3c5d08c65fbf11f941c5e27d9cd460856
Roach <michaelkorblyjunior97@gmail.com>
2024-11-14 09:47:06 +0000

    setup

---
+++ javascript/functions.js
@@ -0,0 +1,13 @@
+var x = document.getElementById("Signin")
+var y = document.getElementById("Signup")
+
+/** creating a function  **/
+function Signup(){
+    x.style.right = "100%";
+    y.style.right = "1%";
+}
+
+function Signin(){
+    x.style.right = "1%";
+    y.style.right = "100%";
+}
